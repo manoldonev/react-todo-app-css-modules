b452cc7b86db632539c59833b6e2d56ae9f069ce
Manol Donev <manol.donev@gmail.com>
2021-10-05 16:42:05 +0300

    refactor: extract header component

---
+++ src/components/TodoList/Header/Header.js
@@ -0,0 +1,8 @@
+import styles from './Header.module.scss';
+
+
+export default function Header() {
+    return (
+        <h1 className={styles.Header}>Things to do</h1>
+    );
+}
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,6 @@
 import TodoItem from './TodoItem';
 import InputToggle from './InputToggle';
+import Header from './Header';
 import Footer from './Footer';
 import Info from './Info';
 import { useTodoState } from '../../context/todo';
@@ -19,12 +20,13 @@ export default function TodoList() {
     return (
         <section className={styles.TodoList}>
             <>
-                <h1 className={styles.TodoList__header}>Things to do</h1>
+                <Header />
                 <InputToggle mode={mode} query={query} />
 
                 <ul className={styles.TodoList__content}>
                     {filteredItems.map(item => <TodoItem key={item.id} data={item} />)}
                 </ul>
+
                 <Footer count={count} filter={filter} />
             </>
             <Info />
